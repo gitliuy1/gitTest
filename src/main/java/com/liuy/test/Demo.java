00c193df1449195b409879aa9d4933a60f2c9b8b
xiaohe <xiaohe@qq.com>
2021-01-18 20:17:04 +0800

    master提交数据

--- src/main/java/com/liuy/test/Demo.java
+++ src/main/java/com/liuy/test/Demo.java
@@ -3,6 +3,8 @@ package com.liuy.test;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("args = " + args);
+
+        System.out.println("master**********");
         System.out.println("dev修改数据");
     }
 }
