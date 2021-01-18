02e3056cdd72efa5f574bd917b2e508751d2880d
xiaohe <xiaohe@qq.com>
2021-01-18 20:15:20 +0800

    dev分支提交数据

--- src/main/java/com/liuy/test/Demo.java
+++ src/main/java/com/liuy/test/Demo.java
@@ -3,5 +3,6 @@ package com.liuy.test;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("args = " + args);
+        System.out.println("dev修改数据");
     }
 }
